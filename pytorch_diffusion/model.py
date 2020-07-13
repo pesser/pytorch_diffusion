48cbcd47b3a3e9e058ca2af8322f3d8a3f36746e
Patrick Esser <Patrick.Esser@gmx.net>
2020-07-13 18:58:54 +0200

    make runnable on gpu

--- pytorch_diffusion/model.py
+++ pytorch_diffusion/model.py
@@ -16,6 +16,7 @@ def get_timestep_embedding(timesteps, embedding_dim):
     half_dim = embedding_dim // 2
     emb = math.log(10000) / (half_dim - 1)
     emb = torch.exp(torch.arange(half_dim, dtype=torch.float32) * -emb)
+    emb = emb.to(device=timesteps.device)
     emb = timesteps.float()[:, None] * emb[None, :]
     emb = torch.cat([torch.sin(emb), torch.cos(emb)], dim=1)
     if embedding_dim % 2 == 1:  # zero pad
